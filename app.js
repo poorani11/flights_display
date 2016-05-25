84255e9c9155bcf518fc67e334c2a967327673b3
poorani11 <poorani.mgr@gmail.com>
2016-05-25 19:06:56 +0530

    Added the landing page for the flights app

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-app.js
+// MODULE
+var flightApp = angular.module('flightApp', ['ngRoute', 'ngResource']);
+
+// ROUTES
+flightApp.config(function ($routeProvider){
+
+    $routeProvider
+
+    .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'homeController'
+    })
+    .when('/flight-display', {
+        templateUrl: 'pages/flight-display.html',
+        controller: 'flightController'
+    })
+});
+
+//SERVICEs
+flightApp.service('seatService', function(){
+
+    this.seats = "29";
+
+});
+
+//FACTORIES
+flightApp.factory('JsonService', function($resource) {
+  return $resource('flights/flights.json',{ }, {
+    getData: {method:'GET', isArray: false}
+  });
+});
+
+// CONTROLLERS
+flightApp.controller('homeController', ['$scope','JsonService','seatService', function($scope,JsonService,seatService){
+    JsonService.query(function(data){
+        console.log(data);
+    $scope.seats = data.Seats_available;
+  });
+    $scope.$watch('seats', function(){
+        seatService.seats = $scope.seats;
+
+    });
+}]);
